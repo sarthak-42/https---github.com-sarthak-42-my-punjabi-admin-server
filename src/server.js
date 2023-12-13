7fdcf3cf4cdf7dddacc5a8f06d878b75ad610d5d
sarthak-42 <sarthakjaiswal4200@gmail.com>
2023-12-13 17:34:32 +0530

    newcommit

--- src/server.js
+++ src/server.js
@@ -19,7 +19,9 @@ const corsOptions = {
   const PORT = process.env.PORT
   
   
-  app.use(cors());
+//   app.use(cors());
+  app.use(cors(corsOptions));
+
   app.use(express.json())
   app.use(express.urlencoded({extended: false}))
   app.use(bodyParser.urlencoded({ extended: false }));
