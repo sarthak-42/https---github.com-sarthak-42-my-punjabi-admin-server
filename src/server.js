9ded81cbac9805a101bec8369b3f2d1d14d60ae7
sarthak-42 <sarthakjaiswal4200@gmail.com>
2023-12-13 15:34:22 +0530

    newUpdate

--- src/server.js
+++ src/server.js
@@ -13,19 +13,13 @@ const PORT = process.env.PORT
 app.use(express.json())
 
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Credentials', 'true'); // If your requests include credentials
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
   
-    if (req.method === 'OPTIONS') {
-      res.sendStatus(200);
-    } else {
-      next();
-    }
-  });
-
+  app.use(cors(corsOptions));
 // const multer = require('multer')
 // const storage = multer.diskStorage({
 //     destination:  (req, file, cb)=> {
