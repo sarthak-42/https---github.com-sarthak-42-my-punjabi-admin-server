34f2e021d7574d37e516280969fe4ad52ff4c9eb
sarthak-42 <sarthakjaiswal4200@gmail.com>
2023-12-14 13:40:50 +0530

    newUpdate

--- src/middlewares/imageUpload.js
+++ src/middlewares/imageUpload.js
@@ -1,13 +1,32 @@
 const multer = require("multer");
-const uploadPath = process.env.UPLOAD_PATH
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadPath); // Adjust the destination directory as needed
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
+const cloudinary = require('cloudinary').v2;
+require('dotenv').config();
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+  });
+
+  const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+      folder: 'your_folder_name',
+      // format: async (req, file) => 'png', // or jpg, jpeg, etc.
+      public_id: (req, file) => `${Date.now()}-${file.originalname}`
+
+    },
+  });
+
+// const uploadPath = process.env.UPLOAD_PATH
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, uploadPath); // Adjust the destination directory as needed
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + "-" + file.originalname);
+//   },
+// });
 
 const upload = multer({ storage: storage });
 // const imageUploadMiddleware = (fieldName) => { 
@@ -29,7 +48,7 @@ const imageUploadMiddleware = (fieldName) => (req, res, next) => {
     upload.single(fieldName)(req, res, (err) => {
       if (err) {
         // Handle multer error
-        console.error("Multer error:", err);
+        console.error("Multer error:", err.stack);
         res.status(500).json({ error: "File upload failed" });
         console.log()
       } else {
